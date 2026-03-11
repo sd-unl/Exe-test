1d0ad3f4d1519306bef3f526f2957b388af66bba
sd-unl <maximum20250302@gmail.com>
2026-03-11 10:55:56 +0200

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <windows.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    MessageBox(NULL, TEXT("Hello, World!"), TEXT("Hello"), MB_OK | MB_ICONINFORMATION);
+    MessageBox(NULL, TEXT("test"), TEXT("Hello"), MB_OK | MB_ICONINFORMATION);
     return 0;
 }
